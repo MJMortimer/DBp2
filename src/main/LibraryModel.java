0a9ab94b8f761e45d8b413660b2b2bba19df1fe9
Matthew Mortimer <matthew.mortimer1@gmail.com>
2014-10-09 01:33:46 +1300

    Book look up and catalogue are good except error checks

--- src/main/LibraryModel.java
+++ src/main/LibraryModel.java
@@ -34,7 +34,11 @@ public class LibraryModel {
 	}
 
 	public String bookLookup(int isbn) {
-		String select =String.format("Select b.isbn, b.title, b.edition_no, b.NumOfCop, b.numLeft, array_to_string(array_agg(trim(BOTH ' ' from a.surname) ORDER BY ba.authorseqno ASC),', ') as authors  From Book b, Author a, Book_Author ba WHERE b.isbn = %d AND b.isbn = ba.isbn AND a.authorid = ba.authorid group by b.isbn ORDER BY b.isbn ASC;", isbn);
+		String select =String.format("SELECT b.isbn, b.title, b.edition_no, b.NumOfCop, b.numLeft, array_to_string(array_agg(trim(BOTH ' ' from a.surname) ORDER BY ba.authorseqno ASC),', ') as authors " 
+									+"FROM Book b LEFT OUTER JOIN Book_Author ba ON (b.isbn = ba.isbn) LEFT OUTER JOIN Author a ON (a.authorid = ba.authorid)"
+									+"WHERE b.isbn = %d "
+									+"GROUP BY b.isbn "
+									+"ORDER BY b.isbn ASC;", isbn);
 		Statement stmt;
 		try {
 			stmt = con.createStatement();
@@ -55,7 +59,10 @@ public class LibraryModel {
 				sb.append(String.format("\t%d: %s\n",res.getInt("ISBN"), res.getString("title")));
 				sb.append(String.format("\tEdition: %d - Number of Copies: %d - Copies Left: %d\n", res.getInt("Edition_No"), res.getInt("NumOfCop"), res.getInt("NumLeft")));
 				String authors = res.getString("authors");
-				sb.append(String.format("\t%s: %s", (authors.contains(",")? "Authors" : "Author"), authors));
+				if(authors.length() == 0)  
+					sb.append("\t(No Authors)"); 
+				else
+					sb.append(String.format("\t%s: %s", (authors.contains(",")? "Authors" : "Author"), authors));
 			}
 			return sb.toString();
 		} catch (SQLException e) {
@@ -66,7 +73,10 @@ public class LibraryModel {
 	}
 
 	public String showCatalogue() {
-		String select =String.format("Select b.isbn, b.title, b.edition_no, b.NumOfCop, b.numLeft, array_to_string(array_agg(trim(BOTH ' ' from a.surname) ORDER BY ba.authorseqno ASC),', ') as authors From Book b, Author a, Book_Author ba WHERE b.isbn = ba.isbn AND a.authorid = ba.authorid group by b.isbn ORDER BY b.isbn ASC;");
+		String select =String.format("SELECT b.isbn, b.title, b.edition_no, b.NumOfCop, b.numLeft, array_to_string(array_agg(trim(BOTH ' ' from a.surname) ORDER BY ba.authorseqno ASC),', ') as authors " 
+				+"FROM Book b LEFT OUTER JOIN Book_Author ba ON (b.isbn = ba.isbn) LEFT OUTER JOIN Author a ON (a.authorid = ba.authorid)"
+				+"GROUP BY b.isbn "
+				+"ORDER BY b.isbn ASC;");
 		Statement stmt;
 		try {
 			stmt = con.createStatement();
@@ -87,8 +97,10 @@ public class LibraryModel {
 				sb.append(String.format("%d: %s\n",res.getInt("ISBN"), res.getString("title")));
 				sb.append(String.format("\tEdition: %d - Number of Copies: %d - Copies Left: %d\n", res.getInt("Edition_No"), res.getInt("NumOfCop"), res.getInt("NumLeft")));
 				String authors = res.getString("authors");
-				sb.append(String.format("\t%s: %s\n", (authors.contains(",")? "Authors" : "Author"), authors));
-			}
+				if(authors.length() == 0)  
+					sb.append("\t(No Authors)\n"); 
+				else
+					sb.append(String.format("\t%s: %s\n", (authors.contains(",")? "Authors" : "Author"), authors));			}
 			return sb.toString();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
