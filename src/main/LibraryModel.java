efef2f447654a3649d000ae8f20a4b1a45208668
Matthew Mortimer <matthew.mortimer1@gmail.com>
2014-10-12 22:12:48 +1300

    Borrow and return looking ok. Need error validations

--- src/main/LibraryModel.java
+++ src/main/LibraryModel.java
@@ -10,10 +10,14 @@ package main;
 
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import static javax.swing.JOptionPane.showMessageDialog;
 
 import javax.swing.*;
 
@@ -35,10 +39,10 @@ public class LibraryModel {
 
 	public String bookLookup(int isbn) {
 		String select =String.format("SELECT b.isbn, b.title, b.edition_no, b.NumOfCop, b.numLeft, array_to_string(array_agg(trim(BOTH ' ' from a.surname) ORDER BY ba.authorseqno ASC),', ') as authors " 
-									+"FROM Book b LEFT OUTER JOIN Book_Author ba ON (b.isbn = ba.isbn) LEFT OUTER JOIN Author a ON (a.authorid = ba.authorid)"
-									+"WHERE b.isbn = %d "
-									+"GROUP BY b.isbn "
-									+"ORDER BY b.isbn ASC;", isbn);
+				+"FROM Book b LEFT OUTER JOIN Book_Author ba ON (b.isbn = ba.isbn) LEFT OUTER JOIN Author a ON (a.authorid = ba.authorid)"
+				+"WHERE b.isbn = %d "
+				+"GROUP BY b.isbn "
+				+"ORDER BY b.isbn ASC;", isbn);
 		Statement stmt;
 		try {
 			stmt = con.createStatement();
@@ -54,7 +58,7 @@ public class LibraryModel {
 			 * ORDER BY b.isbn ASC;
 			 * 
 			 */
-			
+
 			while(res.next()){				
 				sb.append(String.format("\t%d: %s\n",res.getInt("ISBN"), res.getString("title")));
 				sb.append(String.format("\tEdition: %d - Number of Copies: %d - Copies Left: %d\n", res.getInt("Edition_No"), res.getInt("NumOfCop"), res.getInt("NumLeft")));
@@ -92,7 +96,7 @@ public class LibraryModel {
 			 * ORDER BY b.isbn ASC;
 			 * 
 			 */
-			
+
 			while(res.next()){				
 				sb.append(String.format("%d: %s\n",res.getInt("ISBN"), res.getString("title")));
 				sb.append(String.format("\tEdition: %d - Number of Copies: %d - Copies Left: %d\n", res.getInt("Edition_No"), res.getInt("NumOfCop"), res.getInt("NumLeft")));
@@ -114,28 +118,206 @@ public class LibraryModel {
 	}
 
 	public String showAuthor(int authorID) {
-		return "Show Author Stub";
+		String author = String.format("Select AuthorId, trim(BOTH ' ' from Name) as Name, trim(BOTH ' ' from Surname) as Surname FROM Author a Where a.authorId = %d;", authorID);
+		String books = String.format("select * from Book b, Book_Author ba Where b.isbn = ba.isbn AND ba.authorId = %d;", authorID);
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery(author);
+			//CHECK FOR BADNESS
+			StringBuilder sb = new StringBuilder();
+			sb.append("Show Author:"+"\n");
+
+			while(res.next()){				
+				sb.append(String.format("\t%d - %s %s\n",res.getInt("AuthorId"), res.getString("Name"), res.getString("Surname")));
+			}
+			sb.append("\tWritten: \n");
+			stmt = con.createStatement();
+			res = stmt.executeQuery(books);
+			boolean hasBooks = false;
+			while(res.next()){
+				hasBooks = true;
+				sb.append(String.format("\t\t%d - %s\n", res.getInt("isbn"), res.getString("title")));
+			}
+			if(!hasBooks){
+				sb.append("\t\t(No Books)\n");
+			}
+			return sb.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return (e.getMessage());
+		}	
 	}
 
 	public String showAllAuthors() {
-		return "Show All Authors Stub";
+		String authors = String.format("Select AuthorId, trim(BOTH ' ' from Name) as Name, trim(BOTH ' ' from Surname) as Surname FROM Author a;");
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery(authors);
+			//CHECK FOR BADNESS
+			StringBuilder sb = new StringBuilder();
+			sb.append("Show All Authors:"+"\n");
+
+			while(res.next()){				
+				sb.append(String.format("\t%d: %s, %s\n",res.getInt("AuthorId"), res.getString("Surname"), res.getString("Name")));
+			}
+			return sb.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return (e.getMessage());
+		}		
 	}
 
 	public String showCustomer(int customerID) {
-		return "Show Customer Stub";
+		String customer = String.format("Select * FROM Customer c Where c.customerId = %d;", customerID);
+		String books = String.format("select * from Book b, Cust_Book bc  Where b.isbn = bc.isbn AND bc.customerId = %d;", customerID);
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery(customer);
+			//CHECK FOR BADNESS
+			StringBuilder sb = new StringBuilder();
+			sb.append("Show Customer:"+"\n");
+
+			while(res.next()){				
+				sb.append(String.format("\t%d: %s, %s\n",res.getInt("CustomerId"), res.getString("l_name").trim(), res.getString("f_name").trim()));
+			}
+			sb.append("\tBorrowed: \n");
+			stmt = con.createStatement();
+			res = stmt.executeQuery(books);
+			boolean hasBooks = false;
+			while(res.next()){
+				hasBooks = true;
+				sb.append(String.format("\t\t%d - %s\n", res.getInt("isbn"), res.getString("title")));
+			}
+			if(!hasBooks){
+				sb.append("\t\t(No Books)\n");
+			}
+			return sb.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return (e.getMessage());
+		}	
 	}
 
 	public String showAllCustomers() {
-		return "Show All Customers Stub";
+		String customers = String.format("Select * FROM Customer c;");
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery(customers);
+			//CHECK FOR BADNESS
+			StringBuilder sb = new StringBuilder();
+			sb.append("Show All Customers:"+"\n");
+
+			while(res.next()){	
+				String city = res.getString("city");
+				sb.append(String.format("\t%d: %s, %s - %s\n",res.getInt("CustomerId"), res.getString("l_name").trim(), res.getString("f_name").trim(), city == null ? "(no city)": city.trim()));
+			}
+			return sb.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return (e.getMessage());
+		}
 	}
 
 	public String borrowBook(int isbn, int customerID,
 			int day, int month, int year) {
-		return "Borrow Book Stub";
+		try {
+			con.setAutoCommit(false);
+			Statement custStmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			Statement bookStmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			Statement insertStmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+			ResultSet customer = custStmt.executeQuery("SELECT * FROM Customer WHERE CustomerID = "+ customerID +" FOR UPDATE;");
+			if(!customer.next()){
+				con.rollback();
+				con.setAutoCommit(true);
+				System.out.println("No customer exists with ID "+ customerID);
+				return "No customer exists with ID "+ customerID;
+			}
+
+			ResultSet book = bookStmt.executeQuery("SELECT * FROM Book WHERE ISBN = "+ isbn +" FOR UPDATE;");
+			if(!book.next()){
+				con.rollback();
+				con.setAutoCommit(true);
+				System.out.println("No book exists with isbn "+ isbn);
+				return "No book exists with isbn "+ isbn;
+			}
+
+			String insert = String.format("INSERT INTO Cust_Book VALUES (%d, '%d-%d-%d', %d);", isbn, year, month, day, customerID);
+			insertStmt.executeUpdate(insert);
+			showMessageDialog(dialogParent, "The book is soon to be borrowed");
+			book.first();			
+			book.updateInt("numLeft", book.getInt("numleft")-1);
+			book.updateRow();
+			con.commit();
+			con.setAutoCommit(true);
+
+			book.first();
+			customer.first();
+			StringBuilder sb = new StringBuilder();
+			sb.append("Borrow Book\n");
+			sb.append(String.format("\tBook: %d (%s)\n", book.getInt("isbn"), book.getString("title").trim()));
+			sb.append(String.format("\tLoaned to: %d (%s %s)\n",customer.getInt("customerId"),customer.getString("f_name").trim(), customer.getString("l_name").trim()));
+			sb.append(String.format("\tDue Date: %d-%d-%d\n", year, month, day));
+			return sb.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return e.getMessage();
+		}
 	}
 
-	public String returnBook(int isbn, int customerid) {
-		return "Return Book Stub";
+	public String returnBook(int isbn, int customerID) {
+		try {
+			con.setAutoCommit(false);
+			Statement custStmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			Statement bookStmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			Statement deleteStmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+			ResultSet customer = custStmt.executeQuery("SELECT * FROM Customer WHERE CustomerID = "+ customerID +" FOR UPDATE;");
+			if(!customer.next()){
+				con.rollback();
+				con.setAutoCommit(true);
+				System.out.println("No customer exists with ID "+ customerID);
+				return "No customer exists with ID "+ customerID;
+			}
+
+			ResultSet book = bookStmt.executeQuery("SELECT * FROM Book WHERE ISBN = "+ isbn +" FOR UPDATE;");
+			if(!book.next()){
+				con.rollback();
+				con.setAutoCommit(true);
+				System.out.println("No book exists with isbn "+ isbn);
+				return "No book exists with isbn "+ isbn;
+			}
+
+			String delete = String.format("DELETE FROM Cust_Book WHERE isbn = %d AND customerId = %d;", isbn, customerID);
+			deleteStmt.executeUpdate(delete);
+			showMessageDialog(dialogParent, "The book is soon to be returned");
+			book.first();			
+			book.updateInt("numLeft", book.getInt("numleft")+1);
+			book.updateRow();
+			con.commit();
+			con.setAutoCommit(true);
+
+			book.first();
+			customer.first();
+			StringBuilder sb = new StringBuilder();
+			sb.append("Return Book\n");
+			sb.append(String.format("\tBook %d  returned for customer %d\n", book.getInt("isbn"), customer.getInt("customerId")));
+
+			return sb.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return e.getMessage();
+		}
 	}
 
 	public void closeDBConnection() {
